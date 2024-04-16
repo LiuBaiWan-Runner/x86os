2973a42375ce818ad1838ea5b12d432a7eb748e4
LiuBaiWan-XiaoXin <liuzhicheng592@hotmail.com>
2024-04-16 22:01:01 +0800

    Detect available memory capacity

--- start/start/build/source/loader/CMakeFiles/loader.dir/loader_32.c.obj.d
+++ start/start/build/source/loader/CMakeFiles/loader.dir/loader_32.c.obj.d
@@ -1,2 +1,6 @@
 source/loader/CMakeFiles/loader.dir/loader_32.c.obj: \
- E:/OS/x86os/start/start/source/loader/loader_32.c
+ E:/OS/x86os/start/start/source/loader/loader_32.c \
+ E:/OS/x86os/start/start/source/loader/loader.h \
+ E:/OS/x86os/start/start/source/comm/boot_info.h \
+ E:/OS/x86os/start/start/source/comm/types.h \
+ E:/OS/x86os/start/start/source/comm/types.h
--- start/start/source/loader/loader_16.c
+++ start/start/source/loader/loader_16.c
@@ -1,5 +1,10 @@
 __asm__(".code16gcc");
 
+#include "loader.h"
+
+static boot_info_t boot_info;
+
+// 使用内联汇编打印字符串
 static void show_msg (const char *msg){
     char c;
 
@@ -13,7 +18,45 @@ static void show_msg (const char *msg){
         );
     }
 }
+
+// 检测内存容量
+static void detect_memory(void){
+    SMAP_entry_t smap_entry;
+    uint32_t contID = 0, signature, bytes;
+
+    show_msg("Trying to detect memory: ");
+
+    boot_info.ram_region_count = 0;
+    for(int i = 0; i < BOOT_RAM_REGION_MAX; i++){
+        SMAP_entry_t *entry = &smap_entry;
+        __asm__ __volatile__(
+            "int $0x15"
+            :"=a"(signature), "=c"(bytes), "=b"(contID)
+            :"a"(0xE820), "b"(contID), "c"(24), "d"(0x534D4150), "D"(entry));
+        if(signature != 0x534D4150){
+            show_msg("Falied!\n\r");
+            return;
+        }
+        if(bytes > 20 && (entry->ACPI & 0x0001) == 0){
+            // ignore this entry
+            continue;
+        }
+        if(entry->Type == 1){
+            //记录当前可用内存空间的起始地址和大小，使用低32位
+            boot_info.ram_region_cfg[boot_info.ram_region_count].start = entry->BaseL;
+            boot_info.ram_region_cfg[boot_info.ram_region_count].size = entry->LengthL;
+            boot_info.ram_region_count++;
+        }
+        if(contID == 0){
+            //读取结束
+            break;
+        }
+    }
+    show_msg("Successful!\n\r");
+}
+
 void loader_entry(){
     show_msg("......loading......\n\r");
+    detect_memory();
     for(;;){}
 }
